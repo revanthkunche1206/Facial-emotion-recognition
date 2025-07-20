8151609f1c8307cd3237fa7506388eeb3be1c494
revanthkunche1206 <revanthkunche1206@gmail.com>
2025-07-20 12:48:14 +0530

    Django app

---
+++ FER/FacialemotionRecognition/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FacialemotionrecognitionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'FacialemotionRecognition'
